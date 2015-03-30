56bc001cc27abeae701f0606efcdc0a5ed854c3b
TiyeeJiang <tiyee@live.com>
2015-03-30 17:01:15 +0800

    Update redis.class.php

--- application/Config/redis.class.php
+++ application/Config/redis.class.php
@@ -9,8 +9,8 @@
 namespace Config;
 interface redis {
 const RE_HOST = '127.0.0.1';
-/*const RE_USER = 'fb65db74bf1311e4';
-const RE_PASS = 'Jiang1314';*/
+/*const RE_USER = 'xxxxxx';
+const RE_PASS = 'xxxxxx';*/
 const RE_AUTH = '';//$user . ":" . $pwd
 const RE_DBNO = 16;
 const RE_PORT = '6379';
